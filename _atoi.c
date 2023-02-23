27c662783a47ce9d0e9b8b85e3de3994c5bf9302
LQ McDonald <mcdonald.cm7@gmail.com>
2023-02-22 16:48:38 -0800

    Removed DOS line endings

--- _atoi.c
+++ _atoi.c
@@ -2,6 +2,7 @@
 
 /**
  * interactive - returns true if shell is interactive mode
+ *
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
@@ -13,8 +14,10 @@ int interactive(info_t *info)
 
 /**
  * is_delim - checks if character is a delimeter
+ *
  * @c: the char to check
  * @delim: the delimeter string
+ *
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
@@ -26,11 +29,12 @@ int is_delim(char c, char *delim)
 }
 
 /**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: The character to input
+ *
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -40,11 +44,12 @@ int _isalpha(int c)
 }
 
 /**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ * _atoi - converts a string to an integer
+ *
+ * @s: the string to be converted
+ *
+ * Return: 0 if no numbers in string, converted number otherwise
  */
-
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
